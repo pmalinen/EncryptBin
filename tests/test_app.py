38b1bcf0423156a0c174c249f79d8c5d08f3a132
Patrik Malinen <patrik.malinen@gmail.com>
2025-09-21 23:04:08 +0200

    Fixing tests

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,8 +1,13 @@
+import os
+import sys
 import time
 
 from fastapi.testclient import TestClient
 
 from app import app
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
 
 client = TestClient(app)
 
